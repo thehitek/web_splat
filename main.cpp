576926be1338f3a7022b16d6f7ae5e7693896f45
Роман Лапин <lapin.28042003@gmail.com>
2025-03-11 00:34:56 +0300

    setup the simple http server

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 #include <drogon/drogon.h>
 
 int main(int argc, char *argv[]){
+    drogon::app().setLogPath("./")
+         .setLogLevel(trantor::Logger::kWarn)
+         .addListener("0.0.0.0", 80)
+         .setThreadNum(16)
+         .run();
     return 0;
 }
